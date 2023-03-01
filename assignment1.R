408fe17c576d1398d07e01e12f148260a142979a
davgold10 <dgoldsmith@middlebury.edu>
2023-03-02 10:26:43 +1300

    final upload

--- assignment1.R
+++ assignment1.R
@@ -1,4 +1,33 @@
+# Question 1
+## a
+### i
+t <- mean(c(.71, .78, .83))
+t
+### ii
+a <- mean(c(13.6/20, 17/20, 16.6/20, 12.6/20, 12.2/20, 11.6/20, 11/20, 12.4/20))
+a
+### iii
+i <- ((2/3)*t)+((1/3)*a)
+i
+
+### iv
+f <- ((2/3)*0.74) + ((1/3)*i)
+f
+
+## b
+x <- c(15,33,27,9,36,12)
+bMean <- mean(x)
+bMean
+
+bVar <- var(x)
+
+s <- sd(x)
+
+## c
+(22-54)/(s/sqrt(198))
 # Question 2
+
+sum(x) + 90
 
 #### Establishing variables
 
@@ -13,9 +42,10 @@ potato <- (3 * x)+(y)-2
 median(potato)
 
 ## c
+log(40)
 
 ## d
-plot(x,y)
+plot(x,y, main="Basic Scatterplot")
 ?plot
 
 
@@ -37,3 +67,6 @@ var(fun$Height)
 sd(fun$Height)
 ### 1
 funPlot <- hist(fun$Height)
+### 2
+names(funPlot)
+funPlot$counts
